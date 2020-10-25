81a374a38a1b682b55286002e9bddb87c40f950f
Samuel Andert <samuelandert@me.com>
2020-10-25 15:48:26 +0100

    minor fix

--- src/Recycle/Start.svelte
+++ src/Recycle/Start.svelte
@@ -23,26 +23,6 @@
       seinem privaten Omo Haus. Um als Omo Sapien dein Haus zu öffnen oder ein
       neues Omo Sapien Haus zu bauen, melde dich unten an.
     </p>
-    <h1 class="text-center text-3xl font-title text-primary">
-      Willkommen,
-      <br />auf Omo Earth
-    </h1>
-    <p class="text-sm py-4 text-center text-gray-700">
-      Die Menschen auf Omo Earth nennen sich Omo Sapiens. Jeder Omo entwickelt
-      und kontrolliert 100% seiner eigenen Daten und Apps und speichert diese in
-      seinem privaten Omo Haus. Um als Omo Sapien dein Haus zu öffnen oder ein
-      neues Omo Sapien Haus zu bauen, melde dich unten an.
-    </p>
-    <h1 class="text-center text-3xl font-title text-primary">
-      Willkommen,
-      <br />auf Omo Earth
-    </h1>
-    <p class="text-sm py-4 text-center text-gray-700">
-      Die Menschen auf Omo Earth nennen sich Omo Sapiens. Jeder Omo entwickelt
-      und kontrolliert 100% seiner eigenen Daten und Apps und speichert diese in
-      seinem privaten Omo Haus. Um als Omo Sapien dein Haus zu öffnen oder ein
-      neues Omo Sapien Haus zu bauen, melde dich unten an.
-    </p>
   </main>
   <footer class="flex p-4">
     <Route dapp={o.DENTITY} route="login">
